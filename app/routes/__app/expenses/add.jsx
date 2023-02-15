91617e4de27202815d1df1c7133d67400c646e83
Mark Wlodawski <imakethesites@gmail.com>
2023-02-15 17:18:32 -0500

    Submitting forms programmatically and adding visual submission feedback with Form

--- app/routes/__app/expenses/add.jsx
+++ app/routes/__app/expenses/add.jsx
@@ -1,6 +1,9 @@
+import { redirect } from "@remix-run/node";
 import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
+import { addExpense } from "~/data/expenses.server";
+import { validateExpenseInput } from "~/data/validation.server";
 
 export default function AddExpensePage() {
   const navigate = useNavigate();
@@ -12,4 +15,18 @@ export default function AddExpensePage() {
       <ExpenseForm />
     </Modal>
   );
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
+
+  await addExpense(expenseData);
+  return redirect("/expenses");
 }
