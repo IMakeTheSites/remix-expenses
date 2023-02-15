6607d818624c7b964ca84332731c26b74c0ee687
Mark Wlodawski <imakethesites@gmail.com>
2023-02-15 12:09:28 -0500

    Add more expense-related links

--- app/routes/__app/expenses.jsx
+++ app/routes/__app/expenses.jsx
@@ -1,3 +1,5 @@
+import { FaPlus, FaDownload } from "react-icons/fa";
+
 const DUMMY_EXPENSES = [
   {
     id: "e1",
@@ -13,7 +15,7 @@ const DUMMY_EXPENSES = [
   },
 ];
 
-import { Outlet } from "@remix-run/react";
+import { Link, Outlet } from "@remix-run/react";
 import ExpensesList from "~/components/expenses/ExpensesList";
 
 export default function ExpensesLayout() {
@@ -21,6 +23,16 @@ export default function ExpensesLayout() {
     <>
       <Outlet />
       <main>
+        <section id="expenses-actions">
+          <Link to="add">
+            <FaPlus />
+            <span>Add Expense</span>
+          </Link>
+          <a href="/expenses/raw">
+            <FaDownload />
+            <span>Load Raw Data</span>
+          </a>
+        </section>
         <ExpensesList expenses={DUMMY_EXPENSES} />
       </main>
     </>
